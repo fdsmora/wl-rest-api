9250694c81e25657d6ecb8d2efc07ba34d498e09
Fausto Salazar <fausto.ds.mora@gmail.com>
2020-10-31 21:43:38 -0600

    Removed unnecessary code

--- app/weather.py
+++ app/weather.py
@@ -6,10 +6,6 @@ bp = Blueprint('weather', __name__)
 openWeatherAPIKey = '8cb95285a2549c48dfc603cb7bf9e81d'
 weatherEndPointUrl = f'http://api.openweathermap.org/data/2.5/weather?APPID={openWeatherAPIKey}&q='
 
-@bp.route('/hola', methods=('GET',))
-def hola():
-    return 'hola'
-
 @bp.route('/weather/<city>', methods=('GET',))
 @bp.route('/weather/<city>/', methods=('GET',))
 def getWeather(city):
